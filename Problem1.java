c8f2699070c7da201d877a19dd336cd4155e8dd5
Nirdhar Khazanie <ninu@ninu.net>
2016-12-22 12:08:07 -0800

    problem1

---
+++ Problem1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Problem1{
+
+  public static void main(String args[]){
+
+    // running sumtotal
+    int total = 0;
+
+    for(int i=0; i<100; i++){
+    	if(i % 3 == 0){
+    		total += i;
+    	}
+		if(i % 5 == 0){
+			total += i;
+		}
+    }
+	System.out.println(total);
+  }
+}
